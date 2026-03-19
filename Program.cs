3304a169f449d47e232afdce904cf81ded939388
RO_1.5_Saida_Aziza <s.zhakieva24@apec.edu.kz>
2026-03-19 23:06:15 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+int result = 0;
+foreach (int number in numbers)
+{
+    if (number > 0)
+    {
+        result++;
+    }
+}
+Console.WriteLine($"Число элементов больше нуля: {result}");
+
+
+int[] numbers = { -4, -3, -2, -1, 0, 1, 2, 3, 4 };
+
+int n = numbers.Length; // длина массива
+int k = n / 2;          // середина массива
+int temp;               // вспомогательный элемент для обмена значениями
+for (int i = 0; i < k; i++)
+{
+    temp = numbers[i];
+    numbers[i] = numbers[n - i - 1];
+    numbers[n - i - 1] = temp;
+}
+foreach (int i in numbers)
+{
+    Console.Write($"{i} \t");
+}
+
+
+int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
+
+// сортировка
+int temp;
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    for (int j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] > nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
+
+// вывод
+Console.WriteLine("Вывод отсортированного массива");
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.WriteLine(nums[i]);
+}
+
+
+for (int i = 0; i < nums.Length - 1; i++)
+
+    for (int j = i + 1; j < nums.Length; j++)
+
+        if (nums[i] > nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
